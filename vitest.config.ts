3e1a763dd2be47a8604e93737c42a6b9bd7042f5
David Riordan <dr@daveriordan.com>
2023-12-19 11:10:50 -0500

    Update Svelte plugin import and configuration in vitest.config.ts.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vitest/config';
-import svelte from 'vitest-svelte-plugin'; // This is a hypothetical plugin, replace with the actual plugin you are using
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
-  plugins: [
-    svelte(), // Add the Svelte plugin to the Vitest configuration
-  ],
+  plugins: [svelte()],
   test: {
     globals: true,
     environment: 'jsdom',
