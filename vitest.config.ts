0403ef262e3b5994a0402f6a118d1f03261705f6
David Riordan <dr@daveriordan.com>
2023-12-19 11:10:57 -0500

    aider: Update Vitest configuration to preprocess Svelte components with TypeScript using svelte-preprocess.
    # Aider chat conversation:
    
    ASSISTANT: Now that `vitest.config.ts` is a *read-write* file, we can update it to ensure that it correctly handles Svelte components with TypeScript during testing.
    
    We need to configure Vitest to use `svelte-preprocess` for handling `.svelte` files. This is done by adding the `svelte` plugin with the `preprocess` option to the Vitest configuration.
    
    Here's the *SEARCH/REPLACE block* for updating `vitest.config.ts`:
    
    vitest.config.ts
    ```typescript
    <<<<<<< SEARCH
    import { svelte } from "@sveltejs/vite-plugin-svelte";
    =======
    import { svelte } from "@sveltejs/vite-plugin-svelte";
    import sveltePreprocess from 'svelte-preprocess';
    >>>>>>> REPLACE
    ```
    
    vitest.config.ts
    ```typescript
    <<<<<<< SEARCH
      plugins: [svelte()],
    =======
      plugins: [
        svelte({
          preprocess: sveltePreprocess(),
        }),
      ],
    >>>>>>> REPLACE
    ```
    
    After making these changes, your Vitest configuration will preprocess Svelte components with TypeScript during testing. This should resolve the parsing error you encountered when running `npm run coverage`.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vitest/config';
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import sveltePreprocess from 'svelte-preprocess';
 
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte({
+      preprocess: sveltePreprocess(),
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
